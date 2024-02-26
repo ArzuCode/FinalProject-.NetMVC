5144b342909c2dbca8ea8f77ed923f35cf7c4573
Arzu.Code <arzuem@code.edu.az>
2024-02-27 00:00:09 +0400

    Admin panel is created

---
+++ FinalProject-.NetMVC/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rent_a_Car_.Net.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BaseController : Controller
+    {
+    }
+}
