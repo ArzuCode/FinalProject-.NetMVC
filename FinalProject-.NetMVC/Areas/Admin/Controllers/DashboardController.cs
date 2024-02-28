5b58526ec1caa57f20d524f4bc339c3daf4b49cb
Arzu.Code <arzuem@code.edu.az>
2024-02-28 22:52:23 +0400

    creat controllers,views,admin panel update

---
+++ FinalProject-.NetMVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rent_a_Car_.Net.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
